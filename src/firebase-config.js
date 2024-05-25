7b0a66b20a339f9dd4d5851b2ec3072eb6acac86
Francesco Calise <francesco.calise@doit.it>
2024-05-25 12:42:45 +0200

    add env

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -4,14 +4,14 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB27CE_lOHNyC3MtgnWU-hdWJlabJZFPfw",
-    authDomain: "personalgymbook.firebaseapp.com",
-    projectId: "personalgymbook",
-    storageBucket: "personalgymbook.appspot.com",
-    messagingSenderId: "730757104220",
-    appId: "1:730757104220:web:a25fc90f977ad383371c99",
-    measurementId: "G-8092BY6Y3G"
-  };
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+};
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
