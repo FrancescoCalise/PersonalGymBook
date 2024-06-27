23b5e166dad84f621e391ce60649a8a2ce0ffb1e
Francesco Calise <francesco.calise@doit.it>
2024-06-27 11:59:35 +0200

    remove service walker

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,7 +6,6 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { environment } from './environments/environment';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
-import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -14,9 +13,6 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes, withComponentInputBinding()),
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
-    provideFirestore(() => getFirestore()), provideServiceWorker('ngsw-worker.js', {
-            enabled: !isDevMode(),
-            registrationStrategy: 'registerWhenStable:30000'
-          })
+    provideFirestore(() => getFirestore())
   ]
 };
